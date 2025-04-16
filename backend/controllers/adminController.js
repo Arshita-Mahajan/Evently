1a7207b214d69fc3bbcce2c0e61f26425bc1d342
Arshita-Mahajan <156788152+Arshita-Mahajan@users.noreply.github.com>
2025-04-16 15:00:15 +0530

    changed admin controller

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 // Helper function to generate JWT token
-const generateTok = (id) => {
+const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
